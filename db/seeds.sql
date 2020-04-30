adb659c218688321acc8020b29681dee2b29c03c
Quyen Le <heyquyenle@gmail.com>
2020-04-29 18:33:15 -0700

    upload
    finally get to upload all of my works. I still need to work on some functions and the css and mysql and deploy to heroku

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+USE burgers_db
+INSERT INTO burgers (burger_name) VALUES ('Korean Kimchee Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quinoa Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+
+INSERT INTO menu (item) VALUES ('korean Kimchee Burger');
+INSERT INTO menu (item) VALUES ('Avocado Bacon Burger');
+INSERT INTO menu (item) VALUES ('Quinoa Turkey Burger');
+INSERT INTO menu (item) VALUES ('CrispyChicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Vegan Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Blue Cheese Burger');
+INSERT INTO menu (item) VALUES ('Garden Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Deluxe Bison Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Potato Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Curly Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
